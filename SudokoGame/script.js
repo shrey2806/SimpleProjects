641b2a0ac8a8186ce5ce602f1fafe052cc0f63b9
Shrey Agarwal <shrey.ag2806@gmail.com>
2020-04-17 16:17:22 +0530

    Board Initialized and color set

--- SudokoGame/script.js
+++ SudokoGame/script.js
@@ -2,6 +2,19 @@ let button_generate = document.getElementById("GetPuzzle");
 let button_submit = document.getElementById("Submit");
 let button_check = document.getElementById("CheckResult");
 
+var arr = [[],[],[],[],[],[],[],[],[]];
+var board = [[],[],[],[],[],[],[],[],[]];
+var initial_board = [[],[],[],[],[],[],[],[],[]];
+
+for(var i=0;i<9;i++){
+    for(var j=0;j<9;j++){
+        arr[i][j] = document.getElementById(i*9+j);
+    }
+}
+
+
+
+
 
 
 button_generate.onclick = function(){
@@ -11,8 +24,69 @@ button_generate.onclick = function(){
     request.onload = function(){
         var response = JSON.parse(request.response)
         console.log(response);
+        
+        //Populate board;
+        board = response.board;
+        fillInitialBoard(board);
+        setColor();
+        populateBoard(board);
+
     }
     request.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy');
     request.send();
 
 };
+
+function fillInitialBoard(board){
+
+    for(let i=0;i<9;i++){
+        for(let j=0;j<9;j++){
+            if(board[i][j]!=0){
+                initial_board[i][j]=true;
+            }else{
+                initial_board[i][j]=false;
+            }
+        }
+    }
+
+
+}
+
+function setColor(){
+
+    for(let i=0;i<9;i++){
+        for(let j=0;j<9;j++){
+            if(initial_board[i][j]==true){
+                arr[i][j].style.color ="red";
+            }
+        }
+    }
+}
+
+function populateBoard(board){
+
+    for(let i=0;i<9;i++){
+        for(let j=0;j<9;j++){
+            if(board[i][j]!=0){
+                arr[i][j].innerText = board[i][j]; 
+            }else{
+                arr[i][j].innerText = "";
+            }
+        }
+    }
+
+    for(let i=0;i<9;i++){
+
+        for(let j=0;j<9;j++){
+            
+            arr[i][j].onclick = function(){
+
+                arr[i][j].setAttribute("contenteditable","true");
+
+
+            }
+        }
+    }
+
+}
+
