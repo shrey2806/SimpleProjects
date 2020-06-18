ee9b69a7bb1741e147e5d00540f424e26fbf33b4
shrey2806 <shrey.ag2806@gmail.com>
2020-06-18 12:21:10 +0530

    Clean Code

--- TextEditor/script.js
+++ TextEditor/script.js
@@ -1,81 +1,5 @@
-//import Stack from './stack.js';
-class Stack{
+import {Stack} from './stack.js';
 
-    constructor(){
-        this.stack = [];
-        this.size = 0;
-        this.bufferSize = 3;
-
-    }
-
-    isEmpty(){
-        
-        return (this.size===0);
-    
-    }
-
-    top(){
-
-        if( this.isEmpty() === false ) {
-           return this.stack[this.size-1];
-        }
-
-    }
-    push( type , index , val ){
-        
-        if( this.size === 0 ){
-
-            if( type===0 ){
-                this.stack.push([type,index,val]);
-              
-              
-            }
-
-        }
-        else{
-
-            
-            
-            if(this.top()[0]!== type || this.top()[2].length >= this.bufferSize ||
-                this.top()[1] > index){
-            
-                this.stack.push([type,index,val]);
-            
-            }else{
-
-                let ele = this.pop();
-                let newval = val + ele[2];
-                this.stack.push([type,index,newval])
-
-            }
-        }
-
-        this.size++;
-        console.log('stack top is '+ this.top());
-        
-    }
-
-    pop(){
-        
-        if( this.isEmpty() === false ){
-            
-            let ele =  this.stack[this.size-1];
-            this.stack.pop();
-            this.size--;
-            return ele;
-        }
-        else{
-            return [-1,-1,''];
-        }
-        
-
-    }
-    clear(){
-        this.stack = [];
-        this.size = 0;
-    }
-
-}
 
 // Written to prevent default undo behaviour:
 document.onkeydown = function(event){
@@ -93,7 +17,7 @@ onload = function(){
 
 
     
-    text = "";
+    var text = "";
     var stack = new Stack();
 
     textArea.oninput  =function(event){
--- TextEditor/stack.js
+++ TextEditor/stack.js
@@ -1,1 +1,80 @@
 export {Stack}
+
+
+class Stack{
+
+    constructor(){
+        this.stack = [];
+        this.size = 0;
+        this.bufferSize = 3;
+
+    }
+
+    isEmpty(){
+        
+        return (this.size===0);
+    
+    }
+
+    top(){
+
+        if( this.isEmpty() === false ) {
+           return this.stack[this.size-1];
+        }
+
+    }
+    push( type , index , val ){
+        
+        if( this.size === 0 ){
+
+            if( type===0 ){
+                this.stack.push([type,index,val]);
+              
+              
+            }
+
+        }
+        else{
+
+            
+            
+            if(this.top()[0]!== type || this.top()[2].length >= this.bufferSize ||
+                this.top()[1] > index){
+            
+                this.stack.push([type,index,val]);
+            
+            }else{
+
+                let ele = this.pop();
+                let newval = val + ele[2];
+                this.stack.push([type,index,newval])
+
+            }
+        }
+
+        this.size++;
+        console.log('stack top is '+ this.top());
+        
+    }
+
+    pop(){
+        
+        if( this.isEmpty() === false ){
+            
+            let ele =  this.stack[this.size-1];
+            this.stack.pop();
+            this.size--;
+            return ele;
+        }
+        else{
+            return [-1,-1,''];
+        }
+        
+
+    }
+    clear(){
+        this.stack = [];
+        this.size = 0;
+    }
+
+}
